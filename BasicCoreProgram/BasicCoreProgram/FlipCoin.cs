28213fa36690386e408797381a3fc3ffdea5eb81
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-23 17:06:41 +0530

    [Prashant]Add: Flip Coin and print percentage of Heads and Tails

---
+++ BasicCoreProgram/BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class FlipCoin
+    {
+        int headcount = 0;
+        int tailcount = 0;
+        double headpercentage = 0;
+        double tailpercentage = 0;
+        public void Flip()
+        {
+           Console.WriteLine("Enter the value for Number of flips: ");
+           int numberofFlip = Convert.ToInt32(Console.ReadLine());
+
+            if (numberofFlip > 0)
+            {
+                for (int i = 0; i < numberofFlip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(0, 2);
+
+                    if (number == 0)
+                    {
+                        tailcount++;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+                    Console.WriteLine("Tailcount:{0} Headcount:{1}", tailcount, headcount);
+
+                }
+                headpercentage = headcount + 100 / numberofFlip;
+                tailpercentage = tailcount + 100 / numberofFlip;
+                Console.WriteLine("Head Percentage % ==> " + headpercentage);
+                Console.WriteLine("Tail Percentage % ==>" + tailpercentage);
+            }
+            else
+            {
+                Console.WriteLine("The number of Flip is not Valid");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BasicCoreProgram
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Program");
+            Console.WriteLine("Choose Any Options to Perform");
+            Console.WriteLine("1:Flipcoin\n11:Exit");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.Flip();
+                    break;
+                case 11:
+                    Console.WriteLine("Press 11 For exit: ");
+                    break;
+                default:
+                    Console.WriteLine("Choose Vailed Options:");
+                    break;
+            }
+           
+
+        }
+    }
+}
