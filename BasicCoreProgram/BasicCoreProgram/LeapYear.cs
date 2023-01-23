2cc382ffc7b54b6dd32a05dbfb6ccaca6644f531
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-23 17:59:01 +0530

    [Prashant]Add: Find Year is Leap Year or not

---
+++ BasicCoreProgram/BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        int year;
+        public void LeapYearCheck()
+        {
+            Console.WriteLine("Enter the Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("The Year is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("The Year in not a Leap Year");
+            }
+        }
+    }
+}
+                    
+
+          
--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -7,13 +7,17 @@ namespace BasicCoreProgram
         {
             Console.WriteLine("Welcome to Basic Core Program");
             Console.WriteLine("Choose Any Options to Perform");
-            Console.WriteLine("1:Flipcoin\n11:Exit");
+            Console.WriteLine("1:Flipcoin\n2:LeapYear\n11:Exit");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch(Option)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.Flip();
+                    break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.LeapYearCheck();
                     break;
                 case 11:
                     Console.WriteLine("Press 11 For exit: ");
